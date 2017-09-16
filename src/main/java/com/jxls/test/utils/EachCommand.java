a4abe78979f069bee715326f86752d20b2699e86
HelloJavaWorld123 <1806146700@qq.com>
2017-09-16 17:40:00 +0800

    ImageCommand 与 RachelCommand
    添加自定义的遍历命令 以及 图片的命令

--- src/main/java/com/jxls/test/utils/EachCommand.java
+++ src/main/java/com/jxls/test/utils/EachCommand.java
@@ -4,9 +4,16 @@ import org.jxls.area.Area;
 import org.jxls.command.AbstractCommand;
 import org.jxls.command.CellRefGenerator;
 import org.jxls.command.Command;
+import org.jxls.command.SheetNameGenerator;
 import org.jxls.common.CellRef;
 import org.jxls.common.Context;
+import org.jxls.common.JxlsException;
 import org.jxls.common.Size;
+import org.jxls.expression.JexlExpressionEvaluator;
+import org.jxls.util.Util;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created By User: RXK
@@ -17,62 +24,54 @@ import org.jxls.common.Size;
  */
 public class EachCommand extends AbstractCommand
 {
-	public enum  Direction
-	{
-		Right,
-		Down
-	}
-
 	private String var;
 	private String items;
 	private String select;
-	private Area area ;
+	private Area area;
 	private String mutiSheet;
 	private Direction direction = Direction.Down;
 	private CellRefGenerator cellRefGenerator;
 	//如果某一个单元格的数据为空  那么遍历的时候 同样保留一行的格式
 	private String retainEmpty;
-
 	//构造方法重载
-	public EachCommand(String var,String items,Direction direction)
+	public EachCommand(String var, String items, Direction direction)
 	{
 		this.var = var;
 		this.items = items;
 		this.direction = direction == null ? Direction.Down : direction;
 	}
 
-	public EachCommand (String var,String items,Area area)
+	public EachCommand(String var, String items, Area area)
 	{
-		this(var,items,area,Direction.Down);
+		this(var, items, area, Direction.Down);
 	}
 
-	public EachCommand(String var,String items,Area area,Direction direction)
+	public EachCommand(String var, String items, Area area, Direction direction)
 	{
-		this(var,items,direction);
-		if(null != area)
+		this(var, items, direction);
+		if (null != area)
 		{
 			this.area = area;
 			addArea(this.area);
 		}
-
 	}
 
 	@Override
 	public String getName()
 	{
-		return null;
+		return "each";
 	}
 
 	@Override
 	public Command addArea(Area area)
 	{
-		if(null == area)
+		if (null == area)
 		{
 			return this;
 		}
-		if(super.getAreaList().size()>=1)
+		if (super.getAreaList().size() >= 1)
 		{
-			throw  new IllegalArgumentException("You can add only a single area to 'each' command");
+			throw new IllegalArgumentException("You can add only a single area to 'each' command");
 		}
 		this.area = area;
 		return super.addArea(area);
@@ -81,8 +80,80 @@ public class EachCommand extends AbstractCommand
 	@Override
 	public Size applyAt(CellRef cellRef, Context context)
 	{
-		return null;
+		int width = 0, height = 0, index = 0;
+		Collection itemsCollection = Util.transformToCollectionObject(getTransformationConfig().getExpressionEvaluator(), items, context);
+		CellRefGenerator cellRefGenerator = this.cellRefGenerator;
+		if (cellRefGenerator == null && mutiSheet != null)
+		{
+			List<String> sheetNames = getSheetNameList(context);
+			cellRefGenerator = new SheetNameGenerator(sheetNames, cellRef);
+		}
+		CellRef currentCell = cellRefGenerator != null ? cellRefGenerator.generateCellRef(index, context) : cellRef;
+
+		//创建Expression
+		JexlExpressionEvaluator selectExpression = null;
+		if (null != select)
+		{
+			selectExpression = new JexlExpressionEvaluator(select);
+		}
+
+		for (Object obj : itemsCollection)
+		{
+			context.putVar(var, obj);
+			context.putVar(var + "_index", index);
+
+			if (selectExpression != null && !Util.isConditionTrue(selectExpression, context))
+			{
+				context.removeVar(var);
+				context.removeVar(var + "_index");
+				continue;
+			}
+			Size size = area.applyAt(currentCell, context);
+			index++;
+			if (cellRefGenerator != null)
+			{
+				width = Math.max(width, size.getWidth());
+				height = Math.max(height, size.getHeight());
+				if (index < itemsCollection.size())
+				{
+					currentCell = cellRefGenerator.generateCellRef(index, context);
+				}
+			} else if (direction == Direction.Down)
+			{
+				currentCell = new CellRef(currentCell.getSheetName(), currentCell.getRow() + size.getHeight(), currentCell.getCol());
+				width = Math.max(width, size.getWidth());
+				height += size.getHeight();
+			} else
+			{
+				currentCell = new CellRef(currentCell.getSheetName(), currentCell.getRow(), currentCell.getCol() + size.getHeight());
+				width += size.getWidth();
+				height = Math.max(height, size.getHeight());
+			}
+
+			context.removeVar(var);
+			context.removeVar(var + "_index");
+		}
+		if ("true".equalsIgnoreCase(retainEmpty) && width == 0 && height == 0)
+		{
+			return area.applyAt(currentCell, context);
+		}
+
+		return new Size(width, height);
 	}
 
+	private List<String> getSheetNameList(Context context)
+	{
+		try
+		{
+			return (List<String>) context.getVar(mutiSheet);
+		} catch (Exception e)
+		{
+			throw new JxlsException("获取文件名失败：{}," + mutiSheet);
+		}
+	}
 
+	public enum Direction
+	{
+		Right, Down
+	}
 }
--- src/main/java/com/jxls/test/utils/ImageCommand.java
+++ src/main/java/com/jxls/test/utils/ImageCommand.java
@@ -1,5 +1,15 @@
 package com.jxls.test.utils;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.jxls.area.Area;
+import org.jxls.command.AbstractCommand;
+import org.jxls.command.Command;
+import org.jxls.common.*;
+import org.jxls.transform.Transformer;
+import org.jxls.transform.poi.PoiTransformer;
+
 /**
  * Created By User: RXK
  * Date: 2017/9/15
@@ -7,6 +17,111 @@ package com.jxls.test.utils;
  * Version: V1.0
  * Description:
  */
-public class ImageCommand
+public class ImageCommand extends AbstractCommand
 {
+
+
+	private byte[] imageBatys;
+	private ImageType imageType = ImageType.PNG;
+	private Area area;
+
+
+	private String src ;
+	// 当图片不能显示时   显示的 标题
+	private String text ;
+
+	//重载构造方法
+	public ImageCommand(){
+
+	}
+
+	public ImageCommand(byte[] imageBatys,ImageType imageType){
+		this.imageBatys = imageBatys;
+		this.imageType = imageType;
+	}
+
+	public ImageCommand (String image,ImageType imageType){
+		this.src = image ;
+		this.imageType = imageType ;
+	}
+
+	@Override
+	public String getName()
+	{
+		return "image";
+	}
+
+	public String getSrc()
+	{
+		return src;
+	}
+
+	public void setSrc(String src)
+	{
+		this.src = src;
+	}
+
+	public void setImageType(String strImageType){
+		imageType = ImageType.valueOf(strImageType);
+	}
+
+	@Override
+	public Command addArea(Area area){
+		if(super.getAreaList().size()>1){
+			throw new IllegalArgumentException("");
+		}
+		 this.area = area;
+		return super.addArea(area);
+	}
+	@Override
+	public Size applyAt(CellRef cellRef, Context context)
+	{
+		if(null == area)
+		{
+			throw new IllegalArgumentException("NO area for Image Comamd");
+		}
+
+		Transformer transformer =  getTransformer();
+
+		Size  size = area.getSize();
+		size.setWidth(size.getWidth()+1);
+		size.setHeight(size.getHeight()+1);
+		AreaRef areaRef = new AreaRef(cellRef,size);
+		size.setWidth(size.getWidth()-1);
+		size.setHeight(size.getHeight()-1);
+		byte[] imgBatys = imageBatys;
+		if(null != src){
+			Object imageObje = getTransformationConfig().getExpressionEvaluator().evaluate(src, context.toMap());
+			if(imageObje != null){
+				if(!(imageObje instanceof Byte[])){
+					throw new IllegalArgumentException("src value must contain image bytes (byte[])")
+				}
+
+				imgBatys = (byte[])imageObje;
+			}
+			if(imageBatys != null){
+				transformer.addImage(areaRef,imageBatys,imageType);
+			}
+			area.applyAt(cellRef,context);
+			if(null == imageBatys && StringUtils.isNotBlank(text)){
+				PoiTransformer poiTransformer = (PoiTransformer) transformer;
+				Sheet sheet = poiTransformer.getWorkbook().getSheet(cellRef.getSheetName());
+				Row row = sheet.getRow(cellRef.getRow());
+				if(null != row && row.getCell(cellRef.getCol())!= null){
+					row.getCell(cellRef.getCol()).setCellValue(text);
+				}
+			}
+		}
+			return size;
+	}
+
+	public String getText()
+	{
+		return text;
+	}
+
+	public void setText(String text)
+	{
+		this.text = text;
+	}
 }
